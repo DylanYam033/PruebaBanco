35040fd93aa2f61c451190064c245c88005211d5
dylanyam <dayampuezanm@siesa.com>
2024-01-14 13:10:42 -0500

    add logo and colors

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,14 @@
 <aside id="sidebar-wrapper">
+    <br>
     <div class="sidebar-brand">
-        <img class="navbar-brand-full app-header-logo" src="https://i.ibb.co/pwJBq71/logo.png" width="250px" alt="">
+        <img class="navbar-brand-full app-header-logo" src="https://i.ibb.co/tHYhPhH/klipartz-com.png" width="25%" alt="">
     </div>
     <!-- <div class="sidebar-brand sidebar-brand-sm">
         <a href="{{ url('/') }}" class="small-sidebar-text">
             <img class="navbar-brand-full" src="" width="45px" alt="Logo"/>
         </a>
     </div> -->
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    
     <br>
     <ul class="sidebar-menu">
         @include('layouts.menu')
